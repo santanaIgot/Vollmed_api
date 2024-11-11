9d86fe7d8356eed97cc5c8a28ac102caa6b69ae6
santanaIgot <santanaigoor01@gmail.com>
2024-11-10 22:43:27 -0300

    [FEAT] - AUTENTICANDO USUARIO

--- src/main/java/br/com/med/voll/apiVollMed/controller/AutenticacaoController.java
+++ src/main/java/br/com/med/voll/apiVollMed/controller/AutenticacaoController.java
@@ -5,12 +5,10 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.web.bind.annotation.*;
 
-@RestControllerAdvice
+@RestController
 @RequestMapping("/login")
 public class AutenticacaoController {
 
@@ -20,9 +18,11 @@ public class AutenticacaoController {
     //
     @PostMapping
     public ResponseEntity logar(@RequestBody @Valid DadosDtoAutenticacao dto) {
+        var token = new UsernamePasswordAuthenticationToken(dto.login(), dto.senha());
 
+        var authentication = manager.authenticate(token);
 
+        return ResponseEntity.ok().build();
 
-       var authentication = manager.authenticate(token);
     }
 }
--- src/main/java/br/com/med/voll/apiVollMed/model/usuario/Usuario.java
+++ src/main/java/br/com/med/voll/apiVollMed/model/usuario/Usuario.java
@@ -2,17 +2,58 @@ package br.com.med.voll.apiVollMed.model.usuario;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.apache.catalina.User;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
-@Entity()
+import java.util.Collection;
+import java.util.List;
+
+@Entity
 @Table(name = "USUARIO")
 @Getter@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Usuario {
+public class Usuario implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String login;
     private String senha;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return login;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
