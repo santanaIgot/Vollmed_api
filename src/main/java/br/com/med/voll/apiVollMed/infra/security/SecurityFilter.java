b184ba5e6751fd56820b34da776c09bb6b109b87
santanaIgot <santanaigoor01@gmail.com>
2024-11-12 16:04:43 -0300

    [FIX] - CONTROLE DE ACESSO AO USUARIO

--- src/main/java/br/com/med/voll/apiVollMed/infra/security/SecurityFilter.java
+++ src/main/java/br/com/med/voll/apiVollMed/infra/security/SecurityFilter.java
@@ -1,11 +1,15 @@
 package br.com.med.voll.apiVollMed.infra.security;
 
+import br.com.med.voll.apiVollMed.repository.UsuarioRepository;
 import br.com.med.voll.apiVollMed.service.TokenService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
@@ -16,26 +20,26 @@ public class SecurityFilter extends OncePerRequestFilter {
     @Autowired
     private TokenService tokenService;
 
+
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
-        if (request.getRequestURI().equals("/login")) {
-            filterChain.doFilter(request, response);  // Passa a requisição adiante sem fazer verificação de token
-            return;
-        }
-
-
-
+        System.out.println("CHAMANDO FILTRO");
         var tokenJWT = recuperarToken(request);
 
         if (tokenJWT != null) {
+
             try {
                 var subject = tokenService.getSubject(tokenJWT);
-                System.out.println("Subject (usuário autenticado): " + subject);
+                var usuario = usuarioRepository.findByLogin(subject);
+
+                var authentication = new UsernamePasswordAuthenticationToken(usuario, null, usuario.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                        System.out.println("Subject (usuário autenticado): " + subject);
             } catch (RuntimeException e) {
                 System.out.println("Erro na verificação do token: " + e.getMessage());
-                // Opcional: Você pode adicionar lógica para enviar uma resposta de erro se o token for inválido
-                // response.sendError(HttpServletResponse.SC_UNAUTHORIZED, "Token inválido");
-                // return;
             }
         }
 
@@ -46,9 +50,9 @@ public class SecurityFilter extends OncePerRequestFilter {
     private String recuperarToken(HttpServletRequest request) {
         var authorizationHeader = request.getHeader("Authorization");
         System.out.println("Authorization Header: " + authorizationHeader);
-        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ")) {
-            throw new RuntimeException("Token JWT não enviado no cabeçalho Authorization!");
+        if(authorizationHeader != null){
+            return authorizationHeader.replace("Bearer ", "").trim();
         }
-        return authorizationHeader.replace("Bearer ", "").trim();
+    return  null;
     }
 }
